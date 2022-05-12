eacf893305d1f774be7a12b6f20f89a498874b7e
DaniellaZambrano <idaniellazambrano@gmail.com>
2022-05-12 01:26:15 -0400

    make install dependencies

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,3 +3,8 @@ python3 -m $1 venv-diggletters
 . venv-diggletters/bin/activate
 pip install scons
 deactivate
+cd src/
+git clone https://github.com/R3mmurd/DeSiGNAR.git
+cd DeSiGNAR/
+make library
+cd ../../
